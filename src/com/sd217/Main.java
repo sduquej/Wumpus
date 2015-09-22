1886cca2e5a71295c03e32ce447e669a278ec9cb
Sebastián Duque <sduquej@gmail.com>
2015-09-22 09:18:45 +0100

    Initial commit

---
+++ src/com/sd217/Main.java
@@ -0,0 +1,21 @@
+package com.sd217;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        World world = new World(4,20);
+//        world.placeItemInCave(0, 0, World.CaveItem.ADVENTURER);
+//        world.placeItemInCave(0, 1, World.CaveItem.BREEZE);
+//        world.placeItemInCave(0, 2, World.CaveItem.PIT);
+//        world.placeItemInCave(0, 3, World.CaveItem.BREEZE);
+
+//        world.placeItemInCave(1, 0, World.CaveItem.STENCH);
+
+//        world.placeItemInCave(2, 0, World.CaveItem.WUMPUS);
+//        world.placeItemInCave(2, 1, World.CaveItem.TREASURE);
+
+//        world.placeItemInCave(3, 0, World.CaveItem.STENCH);
+        System.out.print(world.toString());
+    }
+}
